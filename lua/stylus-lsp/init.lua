0bbdb5721d64d4dcd30e0d03dc0511b44e6583d0
rdzhumagulov <rdzhumagulov@ivi.ru>
2024-08-06 16:43:28 +0500

    good execute

--- lua/stylus-lsp/init.lua
+++ lua/stylus-lsp/init.lua
@@ -2,9 +2,16 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 
 capabilities.hoverProvider = false
 
-local config_path = vim.fn.expand('<script>:p:h')
+local function get_script_path()
+    local str = debug.getinfo(2, "S").source:sub(2)
+    return str:match("(.*/)")
+end
+
+local config_path = get_script_path()
 
 local server_path = vim.fn.fnamemodify(config_path .. '/../../src/server.ts', ':p')
+
+local global_npm_path = vim.fn.trim(vim.fn.system('npm root -g'))
 
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "stylus",
@@ -15,7 +22,7 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.bo.expandtab = true
         vim.lsp.start({
             name = "stylus_lsp",
-            cmd = { server_path },
+            cmd = {"npx", "--prefix", global_npm_path, "ts-node", server_path, "--stdio" },
             filetypes = { 'stylus' },
             capabilities = capabilities,
             on_attach = function(client, bufnr)
